fa6eccb1e9c9045a094f23f81769690517fb3492
edwinkepler <edwinkepler@protonmail.com>
2017-08-10 00:09:59 +0200

    Add Exercism/cpp/humming

---
+++ Exercism/cpp/hamming/hamming.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+namespace hamming
+{
+    int compute(string a, string b) {
+        if(a.length() != b.length())
+            throw domain_error("Different string sizes");
+
+        int n = 0;
+        for(int i = 0; i < a.length(); i++) {
+            if(a[i] != b[i])
+                n++;
+        }
+
+        return n;
+    }
+}
--- README.md
+++ README.md
@@ -16,6 +16,7 @@ Programs and solutions from tutorials, daily coding routines and such.
   - [Hello, World!](./Exercism/cpp/hello-world)
   - [Leap](./Exercism/cpp/leap)
   - [Gigasecond](./Exercism/cpp/gigasecond)
+  - [Humming](./Exercism/cpp/humming)
 
 ### HackerRank
 
