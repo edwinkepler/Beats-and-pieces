65eca85c5c2619e7a5c80971a6c6f4d63d4400c5
edwinkepler <edwinkepler@protonmail.com>
2017-10-17 12:19:23 +0200

    Add Tutorials/rela-wordl-haskell/ch03/AlgebraicVector.hs

---
+++ Tutorials/haskell-real-world/ch03/AlgebraicVector.hs
@@ -0,0 +1,7 @@
+-- x and y coordinates or lenghts
+data Cartesia2D = Cartesian2D Double Double
+                  deriving (Eq, Show)
+
+-- andle and distance (magnitude)
+data Polar2D = Polar2D Double Double
+               deriving (Eq, Show)
