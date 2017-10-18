558b2547ac579991c5f4350b5aa6462741e16b79
edwinkepler <edwinkepler@protonmail.com>
2017-10-18 10:40:45 +0200

    Update Tutorials/haskell-real-world/ch03/BookStore.hs

--- Tutorials/haskell-real-world/ch03/BookStore.hs
+++ Tutorials/haskell-real-world/ch03/BookStore.hs
@@ -20,4 +20,21 @@ data BillingInfo = CreditCard CardNumber CardHolder Address
                  | Invoice CustomerID
                    deriving (Show)
 
+data Customer = Customer {
+    customerID      :: CustomerID,
+    customerName    :: String,
+    customerAddress :: Address
+} deriving (Show)
 
+customer1 = Customer 2743234 "J.R.R. Tolkien"
+            ["255 Syntax Ct",
+             "Milpitas, CA 90210",
+             "USA"]
+
+customer2 = Customer {
+    customerID      = 2743234,
+    customerAddress = ["255 Syntax Ct",
+                       "Milpitas, CA 90210",
+                       "USA"],
+    customerName    = "J.R.R. Tolkien"
+}
