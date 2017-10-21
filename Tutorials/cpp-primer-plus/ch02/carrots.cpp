65aa5106bf21580435f3ccbcaae972c2671c22b7
edwinkepler <edwinkepler@protonmail.com>
2017-10-21 15:26:51 +0200

    Add Tutorials/cpp-primer-plus/ch02/carrots.cpp

---
+++ Tutorials/cpp-primer-plus/ch02/carrots.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int carrots = 25;
+    cout << "I have " << carrots << " carrots." << endl;
+    carrots--;
+    cout << "Om nom nom. I have " << carrots << "carrots" << endl;
+    return 0;
+}
