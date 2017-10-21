4c4ddf6fa5740c826983e4895b5142de31a21bb5
edwinkepler <edwinkepler@protonmail.com>
2017-10-21 15:27:04 +0200

    Add Tutorials/cpp-primer-plus/ch02/convert.cpp

---
+++ Tutorials/cpp-primer-plus/ch02/convert.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int stonetolb(int);
+
+int main() {
+    int stone;
+    cout << "Enter weight in stone: ";
+    cin >> stone;
+    int pounds = stonetolb(stone);
+    cout << stone << " stone = " << pounds << " pounds." << endl;
+    return 0;
+}
+
+int stonetolb(int sts) {
+    return 14 * sts;
+}
