73017e655ce0b7f0fec3817ddb340513f7b29b92
edwinkepler <edwinkepler@protonmail.com>
2017-10-22 18:58:35 +0200

    Add Tutorials/cpp-primer-plus/ch03/hexoct2.cpp

---
+++ Tutorials/cpp-primer-plus/ch03/hexoct2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int chest = 42;
+    int weist = 42;
+    int inseam = 42;
+
+    cout << "chest = " << chest << " (decimal for 42)" << endl;
+    cout << hex;
+    cout << "weist = " << weist << " (hex for 42)" << endl;
+    cout << oct;
+    cout << "inseam = " << inseam << " (octal for 42)" << endl;
+    return 0;
+}
