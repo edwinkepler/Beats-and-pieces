c9bec8f22335fc8c009f6e90ca39ae26a06ff2b9
edwinkepler <edwinkepler@protonmail.com>
2017-10-22 18:58:09 +0200

    Add Tutorials/cpp-primer-plus/ch03/chartype.cpp

---
+++ Tutorials/cpp-primer-plus/ch03/chartype.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+    cout << "Thanks you for the " << ch << " character." << endl;
+    return 0;
+}
