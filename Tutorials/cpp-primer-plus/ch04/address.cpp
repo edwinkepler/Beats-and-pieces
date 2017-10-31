b648fa506ad0267476d5f3b4f15286398743e32a
edwinkepler <edwinkepler@protonmail.com>
2017-10-31 16:52:27 +0100

    Add Tutorials/cpp-primer-plus/ch04/address.cpp

---
+++ Tutorials/cpp-primer-plus/ch04/address.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int donut = 6;
+    double cups = 4.5;
+
+    cout << "donut value = " << donut << " and adress = " << &donut << endl;
+    cout << "cups value = " << cups << " and adress = " << &cups << endl;
+    return 0;
+}
