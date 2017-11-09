8539abc5a9e330cdd1cc2862bd10cf4d2e1a80ea
edwinkepler <edwinkepler@protonmail.com>
2017-11-09 16:22:23 +0100

    Add Tutorials/rust-programming-language/ch03/variables

---
+++ Tutorials/rust-programming-language/ch03/variables/src/main.rs
@@ -0,0 +1,18 @@
+const MAX_POINTS: u32 = 100000;
+
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("The value of y is: {}", y);
+
+    println!("The value of const MAX_POINTS is: {}", MAX_POINTS);
+
+    let spaces = "     ";
+    let spaces = spaces.len();
+}
