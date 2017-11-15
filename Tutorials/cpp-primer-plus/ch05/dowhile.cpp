d97ad8ca730be47c8d9480c493c8434e78160c3e
edwinkepler <edwinkepler@protonmail.com>
2017-11-15 23:26:21 +0100

    Add Tutorials/cpp-primer-plus/ch05/dowhile.cpp

---
+++ Tutorials/cpp-primer-plus/ch05/dowhile.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter numbers in 1-10range to find my fav number\n";
+    do {
+        cin >> n;
+    } while(n != 7);
+    cout << "Yes, 7 is my fav number\n";
+    return 0;
+}
