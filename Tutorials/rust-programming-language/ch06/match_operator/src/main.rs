1ba6675d1f0e9d97fdd6816c9dbbe220e9d19845
edwinkepler <edwinkepler@protonmail.com>
2017-11-20 17:17:25 +0100

    Add Tutorials/rust-programming-language/ch06/match_operator

---
+++ Tutorials/rust-programming-language/ch06/match_operator/src/main.rs
@@ -0,0 +1,52 @@
+#![allow(unused_variables)]
+fn main() {
+    #[derive(Debug)]
+    enum UsState {
+        Alabama,
+        Alaska,
+    }
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
+
+    fn value_in_cents(coin: Coin) -> u32 {
+        match coin {
+            Coin::Penny => {
+                println!("Lucky penny!");
+                return 1;
+            },
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {:?}!", state);
+                return 25;
+            },
+        }
+    }
+
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alabama)));
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+}
