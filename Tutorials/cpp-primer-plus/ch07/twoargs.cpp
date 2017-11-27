0fc10ea96676a3f2993bf545162ebffc80f374d3
edwinkepler <edwinkepler@protonmail.com>
2017-11-27 13:22:44 +0100

    Add Tutorials/cpp-primer-plus/ch07/twoargs.cpp

---
+++ Tutorials/cpp-primer-plus/ch07/twoargs.cpp
@@ -0,0 +1,29 @@
+// twoargs.cpp -- function with 2 arguments
+#include <iostream>
+using namespace std;
+
+void n_chars(char, int);
+
+int main() {
+    int times;
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+    while(ch != 'q') {
+        cout << "Enter a integer: ";
+        cin >> times;
+        n_chars(ch, times);
+        cout << "\nEnter another character or press 'q' to exit: ";
+        cin >> ch;
+    }
+
+    cout << "The value of times is " << times << endl;
+    return 0;
+}
+
+void n_chars(char c, int n) {
+    while(n-- > 0) {
+        cout << c;
+    }
+}
